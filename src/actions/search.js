033216074f2c10881af14385f959a3a5d4fe2db6
Danting Huang <serena.huangdanting@gmail.com>
2022-04-12 16:55:44 -0400

    Complete defining action creators

--- src/actions/search.js
+++ src/actions/search.js
@@ -5,8 +5,19 @@ import YOUTUBE_API_KEY from '../config/youtube.js';
 
 
 var handleVideoSearch = (q) => {
- 
+
   //TODO:  Write an asynchronous action to handle a video search!
+  return (dispatch) => {
+    searchYouTube({key: YOUTUBE_API_KEY, query: q}, (videos) => {
+      // This function is able to dispatch other action creators
+      dispatch(changeVideoList(videos));
+      dispatch(changeVideo(videos[0]));
+      return;
+    });
+  };
+
+
+
 };
 
 export default handleVideoSearch;
